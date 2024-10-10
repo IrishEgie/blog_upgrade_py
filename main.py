a133cb8bb0cb0d86dee4693ae2d585f669cecf4f
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-10-10 20:44:28 +0800

    first & last: created the repo & created the front-end & backend

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import requests as rq
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    response = rq.get('https://api.npoint.io/674f5423f73deab1e9a7')
+    blog_posts = response.json() #Type dict
+    return render_template('index.html', posts=blog_posts)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog/<int:post_id>')
+def blog(post_id):
+    response = rq.get('https://api.npoint.io/674f5423f73deab1e9a7')
+    response.raise_for_status()
+    blog_posts = response.json()  # Fetch again or cache it
+
+    # Fetch the specific post
+    blog_post = blog_posts[post_id]  # Assuming `post_id` matches the index in the list
+    return render_template('post.html', blog_post=blog_post)
+
+if __name__== "__main__":
+    app.run(debug=True)
