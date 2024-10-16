3556614ca7ece1f792702eead568af6e01b05952
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-10-16 13:54:09 +0800

    update: starting config for blog update

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 import requests as rq
 import smtplib
 import os
@@ -25,11 +34,36 @@ def send_email(sender_msg):
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRETE_KEY')
+Bootstrap5(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
 
 @app.route('/')
 def home():
-    response = rq.get('https://api.npoint.io/674f5423f73deab1e9a7')
-    blog_posts = response.json() #Type dict
+
     return render_template('index.html', posts=blog_posts)
 
 @app.route("/contact", methods=["GET", "POST"])
