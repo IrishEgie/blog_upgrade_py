c6a43d073ee21d603b5e809c144041ef2b2d3819
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-10-19 15:19:26 +0800

    UPDATE: for app & requirements

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('DATABASE_URL', 'sqlite:///posts.db') 
 Bootstrap5(app)
 ckeditor = CKEditor(app)
 login_manager = LoginManager()
